6fb7bf48de4947c10e64cb0c9b39a2a5ec85b3ee
chiennd99 <nguyen-dinh-chien@lline-group.vn>
2024-03-28 22:08:28 +0700

    push code

--- src/layout/HomePage/CustomerSay.tsx
+++ src/layout/HomePage/CustomerSay.tsx
@@ -1,7 +1,11 @@
+// @ts-nocheck
+
 import React from "react";
 import { Carousel, Row, Col, Image, Button } from "antd";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import useTrans from '../useTrans'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const contentStyle: React.CSSProperties = {
   margin: 0,
@@ -17,6 +21,16 @@ const CustomerSay: React.FC = () => {
     console.log(currentSlide);
   };
   const trans = useTrans()
+  const [customer, setCustomer] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("https://web-developing.site/api/customer-messages")
+    .then((response: any) => {
+      setCustomer(response.data.data)
+    })
+
+  }, []);
 
   return (
     <div className="customer">
@@ -39,74 +53,30 @@ const CustomerSay: React.FC = () => {
               />
             }
           >
-            <div>
+            {
+          customer?.map((data, index) => (
+            <div key={index}>
               <Row>
-                <Col lg={12} span={24} className="order-mobile-2" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', color: 'white', paddingRight: '30px'}}>
+                <Col lg={12} span={24} className="order-mobile-2" style={{display: 'flex', flexDirection: 'column', color: 'white', paddingRight: '30px'}}>
                   <p className="customer-tag">“</p>
                   <div className="customer-text">
-                    <p className="customer-desc">
-                      Lorem ipsum dolor sit amet consectetur. Lacinia pulvinar
-                      morbi elit nulla tortor vitae et laoreet. Urna risus elit
-                      enim magna tincidunt nullam cum blandit bibendum. Ornare
-                      orci consequat elementum tincidunt. Dolor cursus faucibus.
-                    </p>
-                    <p className="customer-name">Nam Toong</p>
+                    <p className="customer-desc"dangerouslySetInnerHTML={{ __html: data?.message }} />
+                    <p className="customer-name">{data.name}</p>
                   </div>
                 </Col>
                 <Col lg={12} span={24} className="mobile-order">
                   <Image
                     className="customer-image"
-                    src="/image/customer.png"
+                    src={data.image}
                     alt="Customer"
                     preview={false}
                   />
                 </Col>
               </Row>
             </div>
-            <div>
-              <Row>
-                <Col lg={12} span={24} className="order-mobile-2" >
-                  <p className="customer-tag">“</p>
-                  <p className="customer-desc">
-                    Lorem ipsum dolor sit amet consectetur. Lacinia pulvinar
-                    morbi elit nulla tortor vitae et laoreet. Urna risus elit
-                    enim magna tincidunt nullam cum blandit bibendum. Ornare
-                    orci consequat elementum tincidunt. Dolor cursus faucibus.
-                  </p>
-                  <p className="customer-name">Nam Toong</p>
-                </Col>
-                <Col lg={12} span={24} className="mobile-order">
-                  <Image
-                    className="customer-image"
-                    src="/image/customer.png"
-                    alt="Customer"
-                    preview={false}
-                  />
-                </Col>
-              </Row>
-            </div>
-            <div>
-              <Row>
-                <Col lg={12} span={24} className="order-mobile-2">
-                  <p className="customer-tag">“</p>
-                  <p className="customer-desc">
-                    Lorem ipsum dolor sit amet consectetur. Lacinia pulvinar
-                    morbi elit nulla tortor vitae et laoreet. Urna risus elit
-                    enim magna tincidunt nullam cum blandit bibendum. Ornare
-                    orci consequat elementum tincidunt. Dolor cursus faucibus.
-                  </p>
-                  <p className="customer-name">Nam Toong</p>
-                </Col>
-                <Col lg={12} span={24} className="mobile-order">
-                  <Image
-                    className="customer-image"
-                    src="/image/customer.png"
-                    alt="Customer"
-                    preview={false}
-                  />
-                </Col>
-              </Row>
-            </div>
+            )) 
+          }
+
           </Carousel>
         </div>
       </div>
