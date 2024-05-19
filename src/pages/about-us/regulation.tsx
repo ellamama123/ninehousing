636ff165b4d383837d0b644cd40d0470a32ce78c
chiennd99 <nguyen-dinh-chien@lline-group.vn>
2024-05-19 11:10:38 +0700

    push

--- src/pages/about-us/regulation.tsx
+++ src/pages/about-us/regulation.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Tabs, Button } from "antd";
+import React, { useState, useEffect } from 'react';
+import { Tabs } from "antd";
+import { useRouter } from 'next/router';
 
 const { TabPane } = Tabs;
 
 export default function Regulation() {
+  const [fileContent, setFileContent] = useState('');
+  const { locale } = useRouter()
+
+  useEffect(() => {
+    let link = locale == 'en' ? '/regulation_en.html' : '/regulation_vn.html'
+    fetch(link)
+      .then(response => response.text())
+      .then(html => setFileContent(html))
+      .catch(error => console.error('Error loading file:', error));
+  }, [locale]);
   return (
     <div style={{width: '80%', margin: '0 auto'}}>
       <div>
-        <h1 className="regulation-title" style={{ color: '#FFD600', fontWeight: 'bold', fontSize: '36px'}}>GENERAL TERMS & CONDITIONS IN ACCOMMODATION</h1>
-        <div>
-            <h5 style={{fontSize: '18px', fontWeight: '600'}}>1. CONDITIONS AT START & END OF THE LEASE</h5>
-            <p>At the start of the Lease Contract, the Premise is or shall be delivered to and accepted by the Lessee in its present condition.</p>
-            <p>The Lessee shall surrender the Premise to the Lessor at the end of the Lease Contract in the condition as described at the start of the lease, excepting any subsequent work done by the Lessor and normal ageing during the renting period.</p>
-        </div>
-        <div>
-            <h5 style={{fontSize: '18px', fontWeight: '600'}}>2. USE OF PREMISES</h5>
-            <p>At the start of the Lease Contract, the Premise is or shall be delivered to and accepted by the Lessee in its present condition.</p>
-            <p>The Lessee shall surrender the Premise to the Lessor at the end of the Lease Contract in the condition as described at the start of the lease, excepting any subsequent work done by the Lessor and normal ageing during the renting period.</p>
-        </div>
-
+        <div className="doc-content" dangerouslySetInnerHTML={{ __html: fileContent }} />
       </div>
     </div>
   );
